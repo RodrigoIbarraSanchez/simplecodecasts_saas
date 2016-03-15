4f2721f8fa34d272a125631a183a1fd55471b050
RodrigoIbarraSanchez <lookatthemoon@live.com.mx>
2016-03-14 22:54:39 -0600

    añadiendo mailer y view

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,10 @@
+class ContactMailer < AccionMailer::Base
+	default to: 'rodrigoibarrasanchez@gmail.com'
+
+	def contact_email(name, email, body)
+		@name = name
+		@email = email
+		@body = body
+
+		mail(from: email, subject: "Formulario de contacto <DevSchool>")
+end
